e6bdcbb874a648618fb693aeba578aac24391d40
Jmalone20 <jasmine.malone@patelhs.org>
2020-12-05 04:09:15 +0000

    I added two print statements to go with the assignment operator

--- Main.java
+++ Main.java
@@ -6,8 +6,7 @@ class Main {
     arr1[1] = arr2[1];
     System.out.println(arr1[0]);
     System.out.println(arr1[1]);
-
-  for(int index = 2; index < arr2.length; index++){
+      for(int index = 2; index < arr2.length; index++){
     arr1[index] = arr2[index];
     System.out.println(arr1[index]);
   }  
